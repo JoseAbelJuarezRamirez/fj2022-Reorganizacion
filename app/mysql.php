7cf19a9729df762eb362ca750698429bb182c1ff
JoseAbelJuarezRamirez <19030366@uadeo.mx>
2022-03-29 08:13:57 -0700

    C13
    Definir la clase mysql con las propiedades para conexión

--- app/mysql.php
+++ app/mysql.php
@@ -1,3 +1,13 @@
 <?php 
-    //TODO: CONEXION DE MYSQL
+    use \mysqli;
+
+    class Mysql extends \mysqli {
+        protected $data = [
+            'host' => '127.0.0.1',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 'uadeo',
+            'port' => 3306
+        ];
+    }
 ?>
